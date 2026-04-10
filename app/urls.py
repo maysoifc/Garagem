62f9ba72116c30a9d028bd468be8193cb4e1f2b1
mayara <mayara.soliveiraifc@gmail.com>
2026-04-10 10:01:20 -0300

    FEAT: registrando as rotas

--- app/urls.py
+++ app/urls.py
@@ -12,11 +12,24 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from core.views import UserRegistrationView, UserViewSet
+from core.views import (
+    UserRegistrationView,
+     UserViewSet,
+     AcessorioViewSet,
+     CorViewSet,
+     ModeloViewSet,
+     VeiculoViewSet
+     )
+
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
+
+router.register(r'acessorios', AcessorioViewSet, basename='acessorios')
+router.register(r'cores', CorViewSet, basename='cores')
+router.register(r'modelos', ModeloViewSet, basename='modelos')
+router.register(r'veiculos', VeiculoViewSet, basename='veiculos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
