73d46d323ce826f59ff08db5ede9ec3bd77911a5
mayara <mayara.soliveiraifc@gmail.com>
2026-04-10 09:55:09 -0300

    FEAT: criação arquivo Modelo View e importação init

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -2,3 +2,4 @@ from .user import UserRegistrationView, UserViewSet
 from .veiculo import VeiculoViewSet
 from .acessorio import AcessorioViewSet
 from .cor import CorSerializer
+from .modelo import ModeloViewSet
