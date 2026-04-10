56c379613e5d19215d91ed821665898c6f531998
mayara <mayara.soliveiraifc@gmail.com>
2026-04-10 10:42:45 -0300

    FEAT: Ajeitando plural de cores e acessórios

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -3,5 +3,9 @@ from django.db  import models
 class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Acesorios"
+        verbose_name_plural = "Acessórios"
+
     def __str__(self):
         return f"{self.id} - {self.descricao}"
--- core/models/cor.py
+++ core/models/cor.py
@@ -1,7 +1,13 @@
+from tabnanny import verbose
+
 from django.db import models
 
 class Cor(models.Model):
     nome = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
 
     def __str__(self):
         return f"{self.nome} ({self.id})"
