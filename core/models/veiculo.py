55379c14317f2f4d6a46f4739e0179ea01fa30ae
mayara <mayara.soliveiraifc@gmail.com>
2026-04-10 09:18:28 -0300

    FEAT(veiculo.py): Criação do model veiculo e importação no init

---
+++ core/models/veiculo.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .modelo import Modelo
+from .cor import Cor
+from .acessorio import Acessorio
+
+class Veiculo(models.Model):
+    ano = models.IntegerField(null=True, default=0)
+    preco = models.DecimalField(max_digits=7, decial_places=2, null=True, default=0)
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    acessorio = models.ManyToManyField(Acessorio, related_name="veiculos")
+
+    def __str__(self):
+        return f"{self.id} - {self.modelo} - {self.cor} ({self.ano})"
